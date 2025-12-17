b79b04939c0ad6567b1e03ae827aeb0fe8e81e28
jungsa <106296196+knkevol@users.noreply.github.com>
2025-12-17 15:20:46 +0900

    네트워크 연산

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 
 #include <iostream>
 #include <WinSock2.h>
+#include <string>
 
 #pragma comment(lib, "ws2_32")
 
@@ -26,16 +27,47 @@ int main()
 	connect(ServerSocket, (SOCKADDR*)&ServerSockAddr, sizeof(ServerSockAddr));
 
 	char Buffer[1024] = { 0, };
-	//네트워크에서 바로 받는다.X OS님의 수신 버퍼에서 꺼내온다.
-	int RecvByte = recv(ServerSocket, Buffer, sizeof(Buffer), 0);
-	std::cout << Buffer << std::endl;
 
-	char Message[] = "2222 2212123";
-	//네트워크로 보낸다X. OS님의 송신 버퍼로 보낸다.
-	int SentByte = send(ServerSocket, Message, (int)strlen(Message), 0);
+	while(true)
+	{
+		//네트워크에서 바로 받는다.X OS님의 수신 버퍼에서 꺼내온다.
+		int RecvByte = recv(ServerSocket, Buffer, sizeof(Buffer), 0);
+		std::cout << Buffer << std::endl;
+
+		char Message[10];
+		char Message2[10];
+		char temp;
+		std::cin >> Message;
+		std::cin >> temp;
+		std::cin >> Message2;
+
+		int sum = 0;
+		switch (temp)
+		{
+		case '+':
+			sum = atoi(Message) + atoi(Message2);
+			break;
+		case '-':
+			sum = atoi(Message) - atoi(Message2);
+			break;
+		case '*':
+			sum = atoi(Message) * atoi(Message2);
+			break;
+		case '/':
+			sum = atoi(Message) / atoi(Message2);
+			break;
+
+		}
+
+		std::string sum2 = std::to_string(sum);
+
+		//네트워크로 보낸다X. OS님의 송신 버퍼로 보낸다.
+		int SentByte = send(ServerSocket, sum2.c_str(), (int)strlen(Message), 0);
 
 
-	closesocket(ServerSocket);
+		closesocket(ServerSocket);
+	}
+	
 
 	WSACleanup();
 
